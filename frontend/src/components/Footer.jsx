783dd454074dd7f68e589b7120482cf88d31200b
dishaa-g <disha29gupta@gmail.com>
2024-10-27 02:52:50 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Anokha Zayka</div>
+          <div className="right">
+            <p>Z6 Gulshan Maymar, India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By AK</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
